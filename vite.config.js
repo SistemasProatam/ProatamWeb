11b86058d03daa6a2d0d89529c6ea1f9b455eaa0
Romario Tovar <romariotovar05@gmail.com>
2026-04-28 14:15:00 -0500

    Optimizacion de codigo, comentarios y css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+function stripCommentsPlugin() {
+  return {
+    name: 'strip-html-comments',
+    transformIndexHtml(html) {
+      return html.replace(/<!--[\s\S]*?-->/g, '');
+    }
+  };
+}
+
 export default defineConfig({
+  plugins: [stripCommentsPlugin()],
   build: {
     rollupOptions: {
       input: {
